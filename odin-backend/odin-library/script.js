dc418d2757cfc4a877dff54d4baeef05b09b93d3
vaporbird <ivyan98@gmail.com>
2023-04-25 13:59:39 +0300

    do CSS and some more functionality #TODO transfer functions to methods and format

--- odin-backend/odin-library/script.js
+++ odin-backend/odin-library/script.js
@@ -52,6 +52,7 @@ function Book (title, author, pages, read){
     this.author = author.trim();
     this.title = title.trim();
     this.pages = +pages;
+    this.read = read;
 }
 
 Book.prototype.AddBookToLibrary = function() {
@@ -85,16 +86,20 @@ function DisplayLibrary () {
     LoadLibrary();
     for (book of books){
         const element = document.createElement("div");
+        element.classList += 'book';
 
         const text = document.createElement("span");
         text.innerText = `${book.title} by ${book.author}, ${book.pages} pages `
 
-        const read = document.createElement("checkbox");
-        read.checked = book.read;
+        const read = document.createElement("input");
+        read.type = 'checkbox';
+        read.checked = book.read? true : false;
+        read.addEventListener('click', (e) => {MarkRead(e)})
         
         const remove = document.createElement("button");
         remove.addEventListener('click', (e) => RemoveBook(e));
         remove.innerText = 'X';
+        remove.classList += 'remove-button';
 
         element.appendChild(text);
         element.appendChild(read);
@@ -105,12 +110,60 @@ function DisplayLibrary () {
 }
 
 function RemoveBook(e) {
+    let confirm = false;
+    myConfirmBox("Are you sure you want to delete this book?").then(response=>{
+
+        if (response === true) {
+            const child = e.srcElement.parentNode;
+            const parent = e.srcElement.parentNode.parentNode;
+            const index = Array.prototype.indexOf.call(parent.children, child);
+            books.splice(index, 1);
+            localStorage.setItem("books", JSON.stringify(books));
+            LoadLibrary();
+            DisplayLibrary ();
+        }
+    })
+}
+/* copy from https://www.bracketcounters.com/create-custom-confirm-box-in-javascript */
+let lock = false;
+function myConfirmBox(message) {
+    if(lock === false){
+        lock = true;
+        let element = document.createElement("div");
+        element.classList.add("confirm-book-delete");
+        element.innerHTML = `<div class="box">
+                                ${message}
+                                <div>
+                                    <button id="trueButton" class="btn green">Yes</button> <!-- Set Id for both buttons -->
+                                    <button id="falseButton" class="btn red">No</button>
+                                </div>
+                            </div>`;
+        document.body.appendChild(element);
+        return new Promise(function (resolve, reject) {
+            document.getElementById("trueButton").addEventListener("click", function () {
+                resolve(true);
+                document.body.removeChild(element);
+                lock = false;
+            });
+            document.getElementById("falseButton").addEventListener("click", function () {
+                resolve(false);
+                document.body.removeChild(element);
+                lock = false;
+            });
+        })
+    }
+/*     IDK WHAT IM DOING HERE */
+    return new Promise(function (resolve, reject){
+        resolve(false);
+    })
+}
+
+function MarkRead(e) {
     const child = e.srcElement.parentNode;
     const parent = e.srcElement.parentNode.parentNode;
     const index = Array.prototype.indexOf.call(parent.children, child);
-    books.splice(index, 1);
+    books[index].read = !books[index].read;
     localStorage.setItem("books", JSON.stringify(books));
     LoadLibrary();
     DisplayLibrary ();
-    console.table(books);
 }
